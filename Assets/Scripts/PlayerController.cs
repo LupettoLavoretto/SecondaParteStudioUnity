3b72102bab205d5f0c1055a106f2d99a105c0395
Lupetto Lavoretto <lupettolavoretto@gmail.com>
2024-12-05 11:09:50 +0100

    Chiusura Lesson 3.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float  jumpForce = 10.0f;
+    public float gravityModifier = 0.0f;
+    public bool isOnGround = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        //*= ha la stessa logica di +=, una semplificazione di Physics.gravity = Physics.gravity * gravityModifier;
+        Physics.gravity *= gravityModifier; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            //ForceMode forza l'applicazione della formula, Impulse fa in modo che la cosa sia immediata
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
