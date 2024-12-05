d8b26e56d2b33e436c5dbba1bec5e3532310fedb
Lupetto Lavoretto <LupettoLavoretto@gmail.com>
2024-12-05 18:02:46 +0100

    Chiusura Lesson 3.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim; 
     public float  jumpForce = 10.0f;
     public float gravityModifier = 0.0f;
     public bool isOnGround = true;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         //*= ha la stessa logica di +=, una semplificazione di Physics.gravity = Physics.gravity * gravityModifier;
         Physics.gravity *= gravityModifier; 
     }
@@ -22,10 +24,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             //ForceMode forza l'applicazione della formula, Impulse fa in modo che la cosa sia immediata
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //Per vedere dov'è jump_trig: lo troviamo in animator -> parameters
+            playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
         }
     }
@@ -44,6 +48,10 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            //Queste sono tutte condizioni che troviamo sempre su Animator -> Layers -> Conditions.
+            //Il primo è un booleano, per cui gli diciamo di selezionare la morte "b" come vera. Il secondo invece è un integrale, per cui dobbiamo dargli il valore numerico.
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }    
     }
 }
